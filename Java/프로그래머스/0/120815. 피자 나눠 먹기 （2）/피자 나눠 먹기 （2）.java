a76e018661c1f12097db49ea0a58aa7352cc3ed7
jjwhub788 <jiwonjang221@gmail.com>
2026-04-30 10:25:45 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ Java/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int lcm = lcm(6, n);
-        return lcm / 6;
+        return n / gcd(n, 6);
     }
 
-    // 최대공약수
     public int gcd(int a, int b) {
         while (b != 0) {
             int temp = a % b;
@@ -12,10 +10,5 @@ class Solution {
             b = temp;
         }
         return a;
-    }
-
-    // 최소공배수
-    public int lcm(int a, int b) {
-        return a * b / gcd(a, b);
     }
 }
