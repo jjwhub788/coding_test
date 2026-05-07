1c09dc437bdafe81db1e53a5f04528a644b50009
jjwhub788 <jiwonjang221@gmail.com>
2026-05-07 09:20:52 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 86 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n / 10;           // 서비스 음료 개수
+        answer = (n * 12000) + (k - service) * 2000;
+        return answer;
+    }
+}
