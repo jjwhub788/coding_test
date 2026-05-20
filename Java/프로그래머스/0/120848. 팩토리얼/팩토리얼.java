dee1d75b0761c5cc7624dbef281be14cb388092a
jjwhub788 <jiwonjang221@gmail.com>
2026-05-20 18:11:08 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 62 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int factorial = 1;
+        int i = 1;
+        while (factorial <= n) {
+            i++;
+            factorial *= i;
+        }
+        return i - 1;
+    }
+}
