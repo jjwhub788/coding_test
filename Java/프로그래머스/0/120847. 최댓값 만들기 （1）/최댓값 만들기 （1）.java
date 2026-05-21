ba356496e234f95995d70392b88990ddd4d3d64b
jjwhub788 <jiwonjang221@gmail.com>
2026-05-21 10:16:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.39 ms, Memory: 61.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ Java/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
@@ -8,9 +8,7 @@ class Solution {
         
         // 가장 큰 두 수 곱
         int max1 = numbers[n - 1] * numbers[n - 2];
-        
-        // 가장 작은 두 수 곱 (음수 × 음수 가능성)
-        int max2 = numbers[0] * numbers[1];
-        return Math.max(max1, max2);
+
+        return max1;
     }
 }
